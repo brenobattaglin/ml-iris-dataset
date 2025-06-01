0a2e7f93aa68f7a97f16866449a71b4a945329bc
Breno Battaglin <8771994+brenobattaglin@users.noreply.github.com>
2025-06-01 16:07:05 -0300

    chore: add matplotlib

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,6 @@ pip3 install --upgrade pip
 pip3 install scipy
 pip3 install numpy
 pip3 install scikit-learn
-# Jupyter notebook
+pip3 install matplotlib
 pip3 install ipython
 pip3 install notebook
